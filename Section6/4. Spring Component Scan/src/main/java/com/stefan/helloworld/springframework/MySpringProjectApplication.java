1cc2f69c94e4317bf657014ea35b9a5d71e9cdac
Stefan Roets <stefanroetscode@gmail.com>
2021-07-26 13:49:25 +0200

    6.4Spring Component Scan

---
+++ Section6/4. Spring Component Scan/src/main/java/com/stefan/helloworld/springframework/MySpringProjectApplication.java
@@ -0,0 +1,25 @@
+package com.stefan.helloworld.springframework;
+
+import com.stefan.helloworld.springframework.controllers.MyController;
+import com.stefan.helloworld.springframework.examplebeans.FakeDataSource;
+import com.stefan.helloworld.springframework.examplebeans.FakeJmsBroker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MySpringProjectApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(MySpringProjectApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
+
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getUsername());
+	}
+}
---
+++ Section6/4. Spring Component Scan/src/main/java/com/stefan/helloworld/springframework/config/PropertyConfig.java
@@ -0,0 +1,69 @@
+package com.stefan.helloworld.springframework.config;
+
+import com.stefan.helloworld.springframework.examplebeans.FakeDataSource;
+import com.stefan.helloworld.springframework.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by jt on 6/7/17.
+ */
+@Configuration
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${stefan@.username}")
+    String user;
+
+    @Value("${stefan@.password}")
+    String password;
+
+    @Value("${stefan@.dburl}")
+    String url;
+
+    @Value("${stefan@.jms.username}")
+    String jmsUsername;
+
+    @Value("${stefan@.jms.password}")
+    String jmsPassoword;
+
+    @Value("${stefan@.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassoword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =new PropertySourcesPlaceholderConfigurer();
+        return  propertySourcesPlaceholderConfigurer;
+    }
+}
