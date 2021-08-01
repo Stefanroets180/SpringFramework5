4e9df56be137a2a67837bb3fd749321ca2a768a6
Stefan Roets <stefanroetscode@gmail.com>
2021-08-01 22:40:25 +0200

    section8 going h2 staring up :8080/h2-console

--- Section7/recipe-app/src/main/java/com/roets/stefan/recipeapp/domain/Recipe.java
+++ Section7/recipe-app/src/main/java/com/roets/stefan/recipeapp/domain/Recipe.java
@@ -26,8 +26,14 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -115,5 +121,21 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
---
+++ Section8/recipe-app-going-on/src/main/java/com/roets/stefan/recipeapp/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package com.roets.stefan.recipeapp.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
